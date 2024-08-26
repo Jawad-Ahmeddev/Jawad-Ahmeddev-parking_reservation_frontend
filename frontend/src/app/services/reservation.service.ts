ed8bce2a582755f42c42a29a527ffb16cb19ec5a
Jawad Ahmed Khan <jawadahmed0191@gmail.com>
2024-08-26 17:42:29 +0500

    Initial commit

---
+++ frontend/src/app/services/reservation.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+  private baseUrl = 'http://localhost:2001/api/reservation';
+
+  constructor(private http: HttpClient) { }
+
+  getReservations(): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl);
+  }
+
+  createReservation(data: any): Observable<any> {
+    return this.http.post<any>(this.baseUrl, data);
+  }
+
+  cancelReservation(reservationId: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${reservationId}`);
+  }
+}
