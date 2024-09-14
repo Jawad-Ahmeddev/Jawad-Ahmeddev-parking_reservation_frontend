a3697758b5a32566c5efcbffd7c51ea2ee714024
Jawad-Ahmeddev <140721252+Jawad-Ahmeddev@users.noreply.github.com>
2024-09-15 01:04:02 +0500

    Update reservation.service.ts
    added env variable

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
-  private baseUrl = 'http://localhost:2001/api/reservation';
+  private baseUrl = `${environment.backendUrl}/api/parking`;
 
   constructor(private http: HttpClient) { }
 
