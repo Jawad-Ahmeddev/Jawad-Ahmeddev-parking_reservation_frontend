b60eaaf8abe19a996dcdf2901c00c81663b92f58
Jawad-Ahmeddev <140721252+Jawad-Ahmeddev@users.noreply.github.com>
2024-09-15 00:59:44 +0500

    Update socket.service.ts
    added env variables

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class SocketService {
   private socket: Socket;
 
   constructor() {
-    this.socket = io('http://localhost:2001'); // Update with your backend URL
+    this.socket = io(`${environment.backendUrl}`); // Update with your backend URL
   }
 
   onUpdateParkingStatus(): Observable<void> {
