0a5f84b16df1181a543d0222bd3403b6d62032bd
Jawad Ahmed Khan <140721252+Jawad-Ahmeddev@users.noreply.github.com>
2024-09-15 14:26:03 +0500

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
