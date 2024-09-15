b55a9ed127e71552387e348ce6ad69af2b38f332
Jawad Ahmed Khan <140721252+Jawad-Ahmeddev@users.noreply.github.com>
2024-09-15 13:29:38 +0500

    Update auth.service.ts
    deleted environment import

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
