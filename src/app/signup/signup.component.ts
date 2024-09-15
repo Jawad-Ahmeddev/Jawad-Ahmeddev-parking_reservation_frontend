404f8e9b4e1d21cb40995953b063fb48f037ef92
Jawad Ahmed Khan <140721252+Jawad-Ahmeddev@users.noreply.github.com>
2024-09-15 13:32:35 +0500

    Update signup.component.ts

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-signup',
