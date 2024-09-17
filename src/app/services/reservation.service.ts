4251814e2f4de7139e1d9ab3b4d0bb008aaaf041
Jawad Ahmed Khan <140721252+Jawad-Ahmeddev@users.noreply.github.com>
2024-09-17 16:24:06 +0500

    Update reservation.service.ts

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -8,7 +8,6 @@ import { environment } from 'src/environments/environment';
 })
 export class ReservationService {
   private baseUrl = `${environment.backendUrl}/api/parking`;
-    console.log(`${environment.backendUrl}`)
 
   constructor(private http: HttpClient) { }
 
